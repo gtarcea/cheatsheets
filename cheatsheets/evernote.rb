4622da46806e4896928a15080a15a43e1bf78790
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-10-01 22:13:33 -0400

    Added a cheatsheet for evernote on OSX.

---
+++ cheatsheets/evernote.rb
@@ -0,0 +1,365 @@
+cheatsheet do
+    title 'Evernote'
+    docset_file_name 'Evernote'
+    keyword 'evernote'
+    introduction 'Evernote OSX key bindings'
+    category do
+        id 'Global'
+        entry do
+            command '^+CMD+N'
+            name 'Quick Note'
+        end
+        entry do
+            command '^+ALT+CMD+N'
+            name 'New Note Window'
+        end
+        entry do
+            command '^+CMD+V'
+            name 'Paste to Evernote'
+        end
+        entry do
+            command '^+CMD+C'
+            name 'Clip Rectangle or Window'
+        end
+        entry do
+            command '^+CMD+E'
+            name 'Search in Evernote'
+        end
+    end
+
+    category do
+        id 'Application-Wide'
+        entry do
+            command 'CMD+N'
+            name 'New Note'
+        end
+        entry do
+            command 'ALT+Shift+CMD+N'
+            name 'New Chat'
+        end
+        entry do
+            command 'Shift+CMD+N'
+            name 'New Personal Notebook'
+        end
+        entry do
+            command '^+CMD+T'
+            name 'New Tag'
+        end
+        entry do
+            command 'ALT+CMD+N'
+            name 'New Evernote Window'
+        end
+        entry do
+            command '^+CMD+S'
+            name 'Sync'
+        end
+        entry do
+            command 'CMD+W'
+            name 'Close'
+        end
+        entry do
+            command 'CMD+S'
+            name 'Save'
+        end
+        entry do
+            command 'CMD+Y'
+            name 'Quick Look Attachments'
+        end
+        entry do
+            command 'CMD+P'
+            name 'Print'
+        end
+        entry do
+            command 'ALT+CMD+T'
+            name 'Show/Hide Toolbar'
+        end
+        entry do
+            command 'ALT+CMD+S'
+            name 'Show/Hide Sidebar'
+        end
+        entry do
+            command 'ALT+CMD+0'
+            name 'Work Chat'
+        end
+        entry do
+            command 'ALT+CMD+1'
+            name 'Notes'
+        end
+        entry do
+            command 'ALT+CMD+2'
+            name 'Notebooks'
+        end
+        entry do
+            command 'ALT+CMD+3'
+            name 'Tags'
+        end
+        entry do
+            command 'Shift+CMD+A'
+            name 'Show All Notes'
+        end
+        entry do
+            command 'CMD+J'
+            name 'Jump to Notebook'
+        end
+        entry do
+            command 'Shift+CMD+J'
+            name 'Jump to Tag'
+        end
+        entry do
+            command 'CMD+|'
+            name 'Select Note List'
+        end
+        entry do
+            command 'CMD+['
+            name 'Back'
+        end
+        entry do
+            command 'CMD+]'
+            name 'Forward'
+        end
+        entry do
+            command '^+CMD+F'
+            name 'Enter Full Screen'
+        end
+        entry do
+            command 'CMD+,'
+            name 'Preferences'
+        end
+        entry do
+            command 'CMD+H'
+            name 'Hide Evernote'
+        end
+        entry do
+            command 'ALT+CMD+H'
+            name 'Hide Others'
+        end
+        entry do
+            command 'CMD+Q'
+            name 'Quit Evernote'
+        end
+        entry do
+            command 'CMD+M'
+            name 'Minimize'
+        end
+    end
+
+    category do
+        id 'Note Editor'
+        entry do
+            command 'CMD+Z'
+            name 'Undo'
+        end
+        entry do
+            command 'Shift+CMD+Z'
+            name 'Redo'
+        end
+        entry do
+            command 'CMD+X'
+            name 'Cut'
+        end
+        entry do
+            command 'CMD+C'
+            name 'Copy'
+        end
+        entry do
+            command 'CMD+V'
+            name 'Paste'
+        end
+        entry do
+            command 'Shift+CMD+V'
+            name 'Paste and Match Style'
+        end
+        entry do
+            command 'CMD+A'
+            name 'Select All'
+        end
+        entry do
+            command 'Shift+CMD+X'
+            name 'Encrypt Selected Text'
+        end
+        entry do
+            command 'ALT+CMD+F'
+            name 'Search Notes'
+        end
+        entry do
+            command 'CMD+R'
+            name 'Reset Search'
+        end
+        entry do
+            command 'CMD+F'
+            name 'Find Within Note'
+        end
+        entry do
+            command 'CMD+G'
+            name 'Find Next'
+        end
+        entry do
+            command 'Shift+CMD+G'
+            name 'Find Previous'
+        end
+        entry do
+            command 'CMD+:'
+            name 'Show Spelling and Grammar'
+        end
+        entry do
+            command 'CMD+;'
+            name 'Check Document Now'
+        end
+        entry do
+            command 'fn fn'
+            name 'Start Dictation'
+        end
+        entry do
+            command '^+CMD+Space'
+            name 'Emoji & Symbols'
+        end
+        entry do
+            command 'CMD+/'
+            name 'Copy Public Link'
+        end
+        entry do
+            command 'CMD+Return'
+            name 'Present'
+        end
+        entry do
+            command 'ALT+CMD+Return'
+            name 'Present on Another Screen'
+        end
+        entry do
+            command 'CMD+L'
+            name 'Edit Note Title'
+        end
+        entry do
+            command 'CMD+\''
+            name 'Edit Tags'
+        end
+        entry do
+            command 'Shift+CMD+I'
+            name 'Show/Hide Node Info'
+        end
+        entry do
+            command '^+ALT+CMD+C'
+            name 'Copy Note Link'
+        end
+        entry do
+            command '^+CMD+M'
+            name 'Move To Notebook'
+        end
+        entry do
+            command 'Shift+CMD+M'
+            name 'Merge Notes'
+        end
+        entry do
+            command 'CMD+T'
+            name 'Show Fonts'
+        end
+        entry do
+            command 'Shift+CMD+C'
+            name 'Show Colors'
+        end
+        entry do
+            command 'ALT+CMD+{'
+            name 'Align Left'
+        end
+        entry do
+            command 'ALT+CMD+|'
+            name 'Center'
+        end
+        entry do
+            command 'ALT+CMD+}'
+            name 'Align Right'
+        end
+        entry do
+            command 'Shift+CMD+]'
+            name 'Increase Indent Level'
+        end
+        entry do
+            command 'Shift+CMD+['
+            name 'Decrease Indent Level'
+        end
+        entry do
+            command 'Shift+CMD+U'
+            name 'Toggle Bulleted List'
+        end
+        entry do
+            command 'Shift+CMD+O'
+            name 'Toggle Numbered List'
+        end
+        entry do
+            command 'Tab'
+            name 'Increase List Level'
+        end
+        entry do
+            command 'Shift+Tab'
+            name 'Decrease List Level'
+        end
+        entry do
+            command 'Shift+CMD+L'
+            name 'Insert Table'
+        end
+        entry do
+            command 'CMD+K'
+            name 'Add Link'
+        end
+        entry do
+            command 'Shift+CMD+K'
+            name 'Remove Link'
+        end
+        entry do
+            command 'CMD+B'
+            name 'Bold'
+        end
+        entry do
+            command 'CMD+I'
+            name 'Italic'
+        end
+        entry do
+            command 'CMD+U'
+            name 'Underline'
+        end
+        entry do
+            command '^+CMD+K'
+            name 'Strikethrough'
+        end
+        entry do
+            command '^+CMD+H'
+            name 'Highlight'
+        end
+        entry do
+            command '^+CMD++'
+            name 'Superscript'
+        end
+        entry do
+            command '^+CMD+-'
+            name 'Subscript'
+        end
+        entry do
+            command 'CMD++'
+            name 'Bigger'
+        end
+        entry do
+            command 'CMD+-'
+            name 'Smaller'
+        end
+        entry do
+            command 'Shift+CMD+T'
+            name 'Insert To-do'
+        end
+        entry do
+            command 'Shift+CMD+H'
+            name 'Insert Horizontal Rule'
+        end
+        entry do
+            command 'Shift+CMD+D'
+            name 'Insert Date'
+        end
+        entry do
+            command 'ALT+Shift+CMD+D'
+            name 'Insert Time'
+        end
+        entry do
+            command 'Shift+CMD+F'
+            name 'Simplify Formatting'
+        end
+    end
+end
